49ab9180bc8c034314b34958c47e6bfac1e5a792
Iluzjonista <48825965+Iluzjonista@users.noreply.github.com>
2019-08-22 12:15:02 +0200

    firs-commit

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './styles/main.sass'
+document.body.innerHTML = '<h1>Hello!</h1> <p>I am styled.</p>'
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                use: [
+                    isProduction
+                        ? MiniCssExtractPlugin.loader
+                        : { loader: 'style-loader', },
+                    { loader: 'css-loader', options: { sourceMap: isProduction } },
+                    { loader: 'postcss-loader', options: { sourceMap: isProduction } },
+                    { loader: 'sass-loader', options: { sourceMap: isProduction } }
+                ]
+
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin(),
+    new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output; optional
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+    })
+    ]
+};
+
